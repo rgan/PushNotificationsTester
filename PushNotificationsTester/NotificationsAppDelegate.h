f7f5a2e2ceb5a9c77617de22e252b5303d8f56e8
rgan <rganeshan@2tor.com>
2012-07-22 07:32:04 -0400

    Initial Commit

---
+++ PushNotificationsTester/NotificationsAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  NotificationsAppDelegate.h
+//  PushNotificationsTester
+//
+//  Created by  on 7/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class NotificationsViewController;
+
+@interface NotificationsAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) NotificationsViewController *viewController;
+
+@end
---
+++ PushNotificationsTester/NotificationsViewController.h
@@ -0,0 +1,13 @@
+//
+//  NotificationsViewController.h
+//  PushNotificationsTester
+//
+//  Created by  on 7/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NotificationsViewController : UIViewController
+
+@end
